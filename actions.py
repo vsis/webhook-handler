171aae2aba41877b64d2958beebe2621a0c4787d
Roddy Gonzalez <roddy.gonzalez.89@gmail.com>
2016-10-27 16:16:37 -0300

    Fixed Jenkins object creation

--- actions.py
+++ actions.py
@@ -9,7 +9,7 @@ import settings
 import jenkins
 
 def push(repo, branch, payload):
-    server = jenkins.Jenkins(settings.jenkins_URL, settings.jenkins_user, settings.jenkins_token)
+    server = jenkins.Jenkins(settings.jenkins_URL, username=settings.jenkins_user, password=settings.jenkins_token)
     server.build_job("pep8")
 
 def pull_request(repo, branch, payload):
